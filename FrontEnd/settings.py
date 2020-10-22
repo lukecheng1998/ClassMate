a40a82777fc725f23ba4d7f3ef01e8f8bb1825ed
niyatisriram <niyatisriram@gmail.com>
2020-10-22 01:47:04 -0400

    finished login table

--- FrontEnd/settings.py
+++ FrontEnd/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+#redirect
+LOGIN_REDIRECT_URL = 'home'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
