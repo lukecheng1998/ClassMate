e72b478abfd51ce03a38f3f07e04f7520f86c2f3
lukecheng1998 <lukecheng1998@gmail.com>
2020-12-06 20:01:41 -0800

    attempted to open templates .html

--- FrontEnd/login/loginPrompt.py
+++ FrontEnd/login/loginPrompt.py
@@ -4,35 +4,36 @@ import json
 import requests
 import sys
 from firebase_admin import auth
+class loginPrompt():
+  firebaseConfig = {
+      "apiKey": "AIzaSyDrDF0VPve6Nim45CEw8i4Lyo1L_cQ6Q3s",
+      "authDomain": "classmate-293217.firebaseapp.com",
+      "databaseURL": "https://classmate-293217.firebaseio.com",
+      "projectId": "classmate-293217",
+      "storageBucket": "classmate-293217.appspot.com",
+      "messagingSenderId": "225751399217",
+      "appId": "1:225751399217:web:9a54431dcf0f183a12ce10",
+      "measurementId": "G-VQ21G1H5R1"
+    }
 
-firebaseConfig = {
-    "apiKey": "AIzaSyDrDF0VPve6Nim45CEw8i4Lyo1L_cQ6Q3s",
-    "authDomain": "classmate-293217.firebaseapp.com",
-    "databaseURL": "https://classmate-293217.firebaseio.com",
-    "projectId": "classmate-293217",
-    "storageBucket": "classmate-293217.appspot.com",
-    "messagingSenderId": "225751399217",
-    "appId": "1:225751399217:web:9a54431dcf0f183a12ce10",
-    "measurementId": "G-VQ21G1H5R1"
-  }
 
+  firebase = pyrebase.initialize_app(firebaseConfig)
+  auth1 = firebase.auth()
+  db = firebase.database()
 
-firebase = pyrebase.initialize_app(firebaseConfig)
-auth1 = firebase.auth()
-db = firebase.database()
+  email = sys.argv[1]
+  password = sys.argv[2]
 
-email = sys.argv[1]
-password = sys.argv[2]
+  #user = auth.create_user_with_email_and_password(email, password)
 
-#user = auth.create_user_with_email_and_password(email, password)
-
-login = auth1.sign_in_with_email_and_password(email, password)
-if(login):
-  print("Success")
-  #print(login)
-  print(login['idToken'])
-else:
-  print("Failed")
+  login = auth1.sign_in_with_email_and_password(email, password)
+  if(login):
+    print("Success")
+    #print(login)
+    print(login['idToken'])
+    index = open("../templates/home.html").read()
+  else:
+    print("Failed")
 
 
 
